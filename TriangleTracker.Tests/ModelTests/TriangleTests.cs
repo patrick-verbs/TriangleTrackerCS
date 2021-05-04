edf46826fc95ca03e58aaf135386671ecb26ed22
Patrick Lee <patricklee1138@live.com>
2021-05-04 10:33:23 -0700

    RED: TypeOfTriangle should return 'Isosceles Triangle' if two sides are equal; :recycle: DRY test code
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,18 +6,24 @@ namespace TriangleTracker.TestTools
   [TestClass]
   public class TriangleTests
   {
+    private Triangle _triangleObject = new Triangle();
+
     [TestMethod]
     public void TypeOfTriangle_NotATriangle()
     {
-      Triangle triangleObject = new Triangle();
-      Assert.AreEqual("Not a Triangle", triangleObject.TypeOfTriangle(4, 3, 20));
+      Assert.AreEqual("Not a Triangle", _triangleObject.TypeOfTriangle(4, 3, 20));
     }
 
     [TestMethod]
     public void TypeOfTriangle_Equilateral()
     {
-      Triangle triangleObject = new Triangle();
-      Assert.AreEqual("Equilateral Triangle", triangleObject.TypeOfTriangle(5, 5, 5));
+      Assert.AreEqual("Equilateral Triangle", _triangleObject.TypeOfTriangle(5, 5, 5));
+    }
+
+    [TestMethod]
+    public void TypeOfTriangle_Isosceles()
+    {
+      Assert.AreEqual("Isosceles Triangle", _triangleObject.TypeOfTriangle(3, 3, 5));
     }
   }
 }
